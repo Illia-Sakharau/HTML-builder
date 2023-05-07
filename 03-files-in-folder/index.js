5bce1f7edfe1370b092ed143785cbb9ec8ab3dd7
Illia Sakharau <sakharovia0204@gmail.com>
2023-05-07 19:44:49 +0400

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
+    files.forEach((file) => {
+      if (!file.isDirectory()) { 
+        const filePath = path.join(folderPath, file.name);
+
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const fileName = path.parse(filePath).name;
+            const fileExt = path.parse(filePath).ext.slice(1);
+            const fileSize = stats.size;
+
+            console.log(`${fileName} - ${fileExt} - ${fileSize}b`);
+          }
+        });
+      }
+    });
+  }  
+})
